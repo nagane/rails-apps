a9b149a25a53a781b8204003d3e585c32c262641
nagane <naganeys@gmail.com>
2014-07-26 19:47:51 +0900

    3.2まで終わった

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
-  describe "GET /static_pages" do
-  
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
-  end
+  # 最初から記載されてた
+  #describe "GET /static_pages" do
+  #
+  #  it "works! (now write some real specs)" do
+  #    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+  #    get static_pages_index_path
+  #    response.status.should be(200)
+  #  end
+  #end
   
   describe "Home page" do
 
@@ -19,4 +20,19 @@ describe "StaticPages" do
     end
   end
 
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
 end
