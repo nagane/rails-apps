c6e60514d86bca9011da8baa406e7b7ec310a8b0
nagane <naganeys@gmail.com>
2014-07-28 22:05:05 +0900

    contactページ追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,6 +12,7 @@ describe "StaticPages" do
   #  end
   #end
   
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -21,7 +22,7 @@ describe "StaticPages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
      end
   end
 
@@ -34,7 +35,7 @@ describe "StaticPages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -47,7 +48,20 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
