5bbf5eda5aa58a4d0fc86637ec7c40d860c696d8
nagane <naganeys@gmail.com>
2014-08-04 22:39:18 +0900

    rails generate controller Users new --no-test-framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   match '/about',     to: 'static_pages#about',     via: 'get'
   match '/help',      to: 'static_pages#help',      via: 'get'
