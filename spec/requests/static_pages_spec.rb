8c4d528ae4803263bc1334e175b6d6109becbbe1
nagane <naganeys@gmail.com>
2014-08-04 22:36:01 +0900

    test refactoring

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,66 +2,34 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
-  # 最初から記載されてた
-  #describe "GET /static_pages" do
-  #
-  #  it "works! (now write some real specs)" do
-  #    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-  #    get static_pages_index_path
-  #    response.status.should be(200)
-  #  end
-  #end
-  
+  subject { page }
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      visit root_path
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the title 'Home'" do
-      visit root_path
-      expect(page).to have_title("#{base_title} | Home")
-    end
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the content 'Contact'" do
-      visit contact_path
-      expect(page).to have_content('Contact')
-    end
-
-    it "should have the title 'About Us'" do
-      visit contact_path
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
